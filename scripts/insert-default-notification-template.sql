1fc8b14f4cf03baa892734f750fbf5c5f1ed5d4c
Arslan Tekayev <arslan_t@inbox.ru>
2025-09-10 11:57:22 +0500

    Remove scripts folder from repository - contains one-time setup, testing, and development scripts

--- scripts/insert-default-notification-template.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO notification_templates (name, subject, body) VALUES ('new_transport_request', 'New Transport Request: {requestId}', 'Dear {requestorName},
-
-Your transport request {requestId} has been submitted and is awaiting approval.
-
-Thank you.');
